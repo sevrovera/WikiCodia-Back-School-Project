d30d1f3b78eb1fe94acf5841354f9621de8e2ba1
Juriens Rodrigue <rodrigue.juriens@cgi.com>
2020-05-29 15:53:56 +0200

    effacement des dependances en trop

--- src/main/java/com/example/WikiCodia/controller/UtilisateurController.java
+++ src/main/java/com/example/WikiCodia/controller/UtilisateurController.java
@@ -1,11 +1,8 @@
 package com.example.WikiCodia.controller;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
