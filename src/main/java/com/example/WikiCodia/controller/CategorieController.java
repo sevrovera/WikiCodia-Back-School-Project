f09d0f6b9b99accb11643765841e03358114f7f7
Juriens Rodrigue <rodrigue.juriens@cgi.com>
2020-07-03 11:44:48 +0200

    suppression d imports inutiles

--- src/main/java/com/example/WikiCodia/controller/CategorieController.java
+++ src/main/java/com/example/WikiCodia/controller/CategorieController.java
@@ -15,13 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.WikiCodia.model.Categorie;
-import com.example.WikiCodia.model.Langage;
 import com.example.WikiCodia.repository.CategorieRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
