4ca34ba333769bc2c45c01046ad1b34eaab6ab68
RolloDMF <juriens.rodrigue@gmail.com>
2020-08-08 11:04:59 +0200

    creation utilisateur fonctionne

--- src/main/java/com/example/WikiCodia/controller/UtilisateurController.java
+++ src/main/java/com/example/WikiCodia/controller/UtilisateurController.java
@@ -1,17 +1,17 @@
 package com.example.WikiCodia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.WikiCodia.model.Utilisateur;
 import com.example.WikiCodia.repository.UtilisateurRepository;
 
-@Controller
+@RestController
 @RequestMapping("/utilisateur")
 public class UtilisateurController {
 	
@@ -20,7 +20,8 @@ public class UtilisateurController {
 
 	@RequestMapping(value = "/creation", method = RequestMethod.POST)
 	@ResponseBody
-	public Utilisateur cree(@RequestBody Utilisateur u) {
+	public Utilisateur cree(Utilisateur u) {
+		System.out.println(u);
 		utilisateurRepository.save(u);
 		return u;
 	}
