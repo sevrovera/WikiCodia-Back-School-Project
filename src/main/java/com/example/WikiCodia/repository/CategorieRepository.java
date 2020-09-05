0afdb7504dce8af85b19db570d901b0783f4633b
Juriens Rodrigue <rodrigue.juriens@cgi.com>
2020-09-05 12:48:01 +0200

    on avance doucemnt

--- src/main/java/com/example/WikiCodia/repository/CategorieRepository.java
+++ src/main/java/com/example/WikiCodia/repository/CategorieRepository.java
@@ -3,6 +3,7 @@ package com.example.WikiCodia.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.WikiCodia.model.Categorie;
 
