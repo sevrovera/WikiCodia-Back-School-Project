aace4d6022d0a66cd2fe3583d95d3641eec0aa20
RolloDMF <juriens.rodrigue@gmail.com>
2020-09-07 07:08:35 +0200

    affichage 5 dernier articles

--- src/main/java/com/example/WikiCodia/repository/ArticleRepository.java
+++ src/main/java/com/example/WikiCodia/repository/ArticleRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.WikiCodia.model.Article;
+import com.example.WikiCodia.model.Utilisateur;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom{
 	List<Article> findByTitreContaining(String titre);
 	
-	 @Query("select article from Article article where article.estValide = 0 and article.estPublie = 1")
-	    List<Article> findByIsPublishedAndNotValidated();
+	@Query("select article from Article article where article.estValide = 0 and article.estPublie = 1")
+	List<Article> findByIsPublishedAndNotValidated();
 	 
 	 @Query("Select article from Article article where article.estPromu = 1")
 	 	List<Article> findPromotedArticles();
@@ -20,4 +21,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long>, Article
 	 
 	 List<Article> findNewerArticles();
 
+	@Query("Select article from Article article where article.estPromu = 1")
+	List<Article> findPomotedArticles();
+		 
+	List<Article> findFirst5ByAuteur(Utilisateur auteur);
 }
